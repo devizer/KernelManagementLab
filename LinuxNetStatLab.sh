98df0741e4f58f411b27cbdbfa3184ed19f74ed5
devizer <devizer@users.github.com>
2019-02-08 23:13:15 +0200

    updated

--- LinuxNetStatLab.sh
+++ LinuxNetStatLab.sh
@@ -5,6 +5,6 @@ git clone https://github.com/devizer/KernelManagementLab
 cd KernelManagementLab
 git pull
 cd LinuxNetStatLab
-msbuild /t:Rebuild /p:Configuration=Debug; cd bin/Debug; mono LinuxNetStatLab.exe 538
+msbuild /t:Rebuild /p:Configuration=Debug; cd bin/Debug; pdb2mdb LinuxNetStatLab.exe; mono LinuxNetStatLab.exe 538
 
 
