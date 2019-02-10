728610ddf5fded3a6e9783b2da5778af6110b6b6
devizer <devizer@users.github.com>
2019-02-10 11:47:14 +0200

    updated

--- Linux-Mount-Lab.sh
+++ Linux-Mount-Lab.sh
@@ -3,9 +3,8 @@ cd $HOME; \
 rm -rf $work; \
 git clone https://github.com/devizer/KernelManagementLab; \
 cd KernelManagementLab; \
- 
 git pull; \
 nuget restore *.sln; \
 xbuild /t:Rebuild /p:Configuration=Debug /v:m; \
 cd MountLab/bin/Debug; \
-pdb2mdb MountLab.exe; mono MountLab.exe
+pdb2mdb KernelManagementJam.dll; mono MountLab.exe
