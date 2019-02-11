faf1c0622ddce5d2ec7324682eb018399661a0a1
devizer <devizer@users.github.com>
2019-02-11 02:43:00 +0200

    updated

--- MountLab/Program.cs
+++ MountLab/Program.cs
@@ -98,7 +98,19 @@ namespace MountLab
     {
         public static void Dump(object anObject, string fileName)
         {
-            string json = JsonConvert.SerializeObject(anObject, Formatting.Indented);
+
+            JsonSerializer ser = new JsonSerializer()
+            {
+                Formatting = Formatting.Indented,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            };
+
+            StringBuilder json = new StringBuilder();
+            StringWriter jwr = new StringWriter(json);
+            ser.Serialize(jwr, anObject);
+            jwr.Flush();
+
+            // string json = JsonConvert.SerializeObject(anObject, Formatting.Indented, settings);
             using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
             using (StreamWriter wr = new StreamWriter(fs, new UTF8Encoding(false)))
             {
