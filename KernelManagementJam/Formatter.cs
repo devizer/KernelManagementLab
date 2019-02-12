d63ee20217e770368fae1cacc46e308e4b1d99a7
devizer <devizer@users.github.com>
2019-02-12 09:52:52 +0200

    updated

--- KernelManagementJam/Formatter.cs
+++ KernelManagementJam/Formatter.cs
@@ -17,7 +17,7 @@
                 return (number / 1024d / 1024d).ToString("0.#") + "M";
 
             if (number < 9999999999999)
-                return (number / 1024d / 1024d).ToString("0.#") + "M";
+                return (number / 1024d / 1024d / 1024d).ToString("0.#") + "G";
 
             return (number / 1024d / 1024d / 1024d / 1024d).ToString("0.#") + "T";
         }
