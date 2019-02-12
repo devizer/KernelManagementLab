61deeca007aa94041adff1335958d70a4c7a910d
devizer <devizer@users.github.com>
2019-02-12 10:11:11 +0200

    updated

--- MountLab/Program.cs
+++ MountLab/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using KernelManagementJam;
 using Mono.Unix;
 
@@ -97,82 +94,6 @@ namespace MountLab
 
             DebugDumper.Dump(analyz, "ProcMountsAnalyzer.js");
 
-        }
-
-        private static void DumpProcMounts_Obsolete()
-        {
-            bool isWin = Environment.OSVersion.Platform == PlatformID.Win32NT;
-            IList<MountEntry> mounts = ProcMountsParser.Parse(isWin ? "mounts" : "/proc/mounts").Entries;
-                ConsoleTable report = new ConsoleTable(
-                    "Device", "FS", "Path", // from /proc/mounts
-                    "", "-Free", "-Total", "Label", "msec",""
-                );
-
-                foreach (var mount in mounts)
-                {
-                    Exception error = null;
-                    string driveInfo = null;
-                    Stopwatch sw = Stopwatch.StartNew();
-                    DriveDetails details = null;
-                    try
-                    {
-                        // TRY System.IO.DriveInfo
-                        var di = new DriveInfo(mount.MountPath);
-                        details = new DriveDetails()
-                        {
-                            MountEntry = mount,
-                            IsReady = di.IsReady,
-                            Format = di.DriveType.ToString(),
-                            FreeSpace = di.TotalFreeSpace,
-                            TotalSize = di.TotalSize,
-                        };
-
-                    }
-                    catch (Exception exNet)
-                    {
-                        error = exNet;
-                        // TRY UnixDriveInfo 
-                        try
-                        {
-                            UnixDriveInfo di = new UnixDriveInfo(mount.MountPath);
-                            details = new DriveDetails()
-                            {
-                                MountEntry = mount,
-                                IsReady = di.IsReady && di.TotalSize > 0,
-                                Format = di.DriveType.ToString(),
-                                FreeSpace = di.TotalFreeSpace,
-                                TotalSize = di.TotalSize,
-                            };
-                            error = null;
-                        }
-                        catch (Exception exUnix)
-                        {
-                            // error = exUnix;
-                        }
-                    }
-
-                    double msec = sw.ElapsedTicks * 1000d / Stopwatch.Frequency;
-                    if (details != null)
-                    {
-                        report.AddRow(
-                            mount.Device, mount.FileSystem, mount.MountPath,
-                            details.IsReady ? "OK" : "--", Formatter.FormatBytes(details.FreeSpace), Formatter.FormatBytes(details.TotalSize),
-                            details.Format, $"{msec:f2}"
-                        );
-                    }
-                    else if (error != null)
-                    {
-                        string errorInfo = error.GetType().Name + ": " + error.Message.Replace(Environment.NewLine, " ");
-                        report.AddRow(
-                            mount.Device, mount.FileSystem, mount.MountPath,
-                            "--", "", "",
-                            "", $"{msec:f2}", errorInfo
-                        );
-                    }
-
-                }
-
-                Console.WriteLine(report);
         }
 
         static void DumpUnixDrives()
