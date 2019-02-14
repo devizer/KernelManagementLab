c12c1dba0bae499efb999ede33be299ddf0a63d5
devizer <devizer@users.github.com>
2019-02-15 01:17:43 +0200

    updated

--- KernelManagementJam/SmallFileReader.cs
+++ KernelManagementJam/SmallFileReader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
@@ -28,6 +29,40 @@ namespace KernelManagementJam
         }
 
         public static string ReadFirstLine(string fileName)
+        {
+            try
+            {
+                using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (var rdr = new StreamReader(fs, FileEncoding))
+                {
+                    var ret = rdr.ReadLine();
+                    return ret;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+            if (!File.Exists(fileName))
+            {
+                AppendSingleLinerLog(string.Format("[{0}] single-line file not found", fileName));
+                return null;
+            }
+
+            using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var rdr = new StreamReader(fs, FileEncoding))
+            {
+                var ret = rdr.ReadLine();
+
+                var logLine = string.Format("[{0}] first line: '{1}'", fileName, ret);
+                AppendSingleLinerLog(logLine);
+
+                return ret;
+            }
+        }
+
+        public static string ReadFirstLine_Slow(string fileName)
         {
             if (!File.Exists(fileName))
             {
