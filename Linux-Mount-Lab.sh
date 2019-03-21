9b9d1a4ae23aa49d2da33f3cb4d3d54bf8d4a82f
devizer <devizer@users.github.com>
2019-03-21 23:14:31 +0200

    updating

--- Linux-Mount-Lab.sh
+++ Linux-Mount-Lab.sh
@@ -10,6 +10,7 @@ if [ "$(command -v msbuild)" == "" ]; then cmd=xbuild; else cmd=msbuild; fi; ech
 eval time xbuild /t:Rebuild /p:Configuration=Release /v:m; \
 cd MountLab/bin/Release; pdb2mdb *.exe *.dll; \
 bash repack.sh; \
-cd .; mono --profile=log:noalloc MountLab.exe Monitor-V1
+cd .; mono  MountLab.exe Monitor-V1
 
 # pdb2mdb KernelManagementJam.dll; mono MountLab.exe Monitor-V1
+# --profile=log:noalloc
