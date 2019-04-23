be5d117fe4c5eab71111e1246a9de745d83a6955
devizer <devizer@github.io>
2019-04-23 04:48:05 +0300

    makeup

--- Universe.W3Top/CompressedMimeTypes.cs
+++ Universe.W3Top/CompressedMimeTypes.cs
@@ -13,7 +13,7 @@ namespace ReactGraphLab
         private static string[] Build()
         {
             string raw = @"text/plain text/x-component text/css text/xml text/javascript
-   application/atom+xml application/rss+xml application/vnd.ms-fontobject
+   application/atom+xml application/rss+xml application/vnd.ms-fontobject font/woff2
    application/x-font-ttf application/x-web-app-manifest+json application/xhtml+xml application/xml
    application/json application/javascript font/opentype image/svg+xml image/x-icon
    application/xml application/xml+rss";
