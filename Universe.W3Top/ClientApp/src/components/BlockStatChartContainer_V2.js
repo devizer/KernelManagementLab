722b90bff2a19c55789cc2dfa930afe1f70f4391
devizer <devizer@github.io>
2019-04-25 21:22:57 +0300

    Muted chart updating when app is minimized, on background tab or screen is locked. fixed memory leaks

--- Universe.W3Top/ClientApp/src/components/BlockStatChartContainer_V2.js
+++ Universe.W3Top/ClientApp/src/components/BlockStatChartContainer_V2.js
@@ -73,7 +73,7 @@ export class BlockChartContainer_V2 extends Component {
 
 
     renderLoading() {
-        let style = {textAlign: "center", paddingTop: NetDevChart.ChartSize.height/2, width: NetDevChart.ChartSize.width};
+        let style = {textAlign: "center", paddingBottom: NetDevChart.ChartSize.height/2, paddingTop: NetDevChart.ChartSize.height/2, width: NetDevChart.ChartSize.width};
         return (
             <h6 id="BlockCharts_is_Waiting_for_connection" style={style}>
                 Waiting for curved lines
