ce426f19a6da94c60f114f3147f37058c89c99c0
devizer <devizer@github.io>
2019-05-21 04:43:52 +0300

    Updated tests for DataGenerator.cs and its flavours

--- KernelManagementJam.Tests/DataGeneratorTests.cs
+++ KernelManagementJam.Tests/DataGeneratorTests.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using KernelManagementJam.Benchmarks;
 using NUnit.Framework;
@@ -11,7 +13,7 @@ namespace KernelManagementJam.Tests
     {
         [Test]
         [TestCaseSourceAttribute(typeof(DataGeneratorTests), nameof(GetAllFlavour))]
-        public void All(DataFlavour fla)
+        public void Perform(DataFlavour fla)
         {
             Console.WriteLine($"Flavour : {fla}");
             // return;
@@ -19,9 +21,44 @@ namespace KernelManagementJam.Tests
             var sizes = new[] {1, 2, 3, 4, 8, 16, 1024 * 1024, 128 * 1024 * 1024};
             foreach (var size in sizes)
             {
-                DataGenerator dg = new DataGenerator(fla);
-                byte[] bytes = new byte[size];
-                dg.NextBytes(bytes);
+                var bytes = Generate(fla, size);
+            }
+        }
+
+        [Test]
+        [TestCaseSourceAttribute(typeof(DataGeneratorTests), nameof(GetAllFlavour))]
+        public void Measure(DataFlavour fla)
+        {
+            Console.WriteLine($"Flavour : {fla}");
+            // return;
+
+            var sizes = new[] {1, 2, 3, 4, 8, 16, 1024 * 1024, 128 * 1024 * 1024};
+            foreach (var size in sizes)
+            {
+                var bytes = Generate(fla, size);
+                int len = GetCompressedSize(bytes);
+                Console.WriteLine($"{size:n0} --> {len:n0}, {(len * 100.000 / size):f1}%");
+            }
+        }
+
+        private static byte[] Generate(DataFlavour fla, int size)
+        {
+            DataGenerator dg = new DataGenerator(fla);
+            byte[] bytes = new byte[size];
+            dg.NextBytes(bytes);
+            return bytes;
+        }
+
+        static int GetCompressedSize(byte[] arg)
+        {
+            using (MemoryStream mem = new MemoryStream())
+            using (DeflateStream pack = new DeflateStream(mem, CompressionLevel.Optimal))
+            using (BufferedStream buf = new BufferedStream(pack, 65536))
+            {
+                buf.Write(arg, 0, arg.Length);
+                buf.Flush();
+                pack.Flush();
+                return (int) mem.Length;
             }
         }
 
