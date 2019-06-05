07fb6982db50dc6b3c4c408d9486eeea9ba41021
devizer <devizer@github.io>
2019-06-05 20:25:51 +0300

    Tested start disk benchmark

---
+++ Universe.Dashboard.DAL.Tests/DiskBenchmarkEntityTests.cs
@@ -0,0 +1,67 @@
+using System;
+using KernelManagementJam.Benchmarks;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Universe.Benchmark.DiskBench;
+using Universe.Dashboard.DAL;
+
+namespace Tests
+{
+    public class DiskBenchmarkEntityTests
+    {
+        static DashboardContext CreateDbContext() => DbEnv.CreateDbContext();
+        
+        [SetUp]
+        public void Setup()
+        {
+            DashboardContext context = CreateDbContext();
+            context.Database.Migrate();
+        }
+
+        [Test]
+        public void TestEmpty()
+        {
+            DashboardContext context = CreateDbContext();
+            context.DiskBenchmark.Add(new DiskBenchmarkEntity() { CreatedAt = DateTime.UtcNow, MountPath = "/test-empty"});
+            context.SaveChanges();
+        }
+
+        [Test]
+        public void TestArguments()
+        {
+            DashboardContext context = CreateDbContext();
+            DiskBenchmark b = new DiskBenchmark("/test-args");
+            var entity = new DiskBenchmarkEntity() { CreatedAt = DateTime.UtcNow, MountPath = b.Parameters.WorkFolder};
+            entity.Args = b.Parameters;
+            context.DiskBenchmark.Add(entity);
+            context.SaveChanges();
+        }
+
+        [Test]
+        public void TestEmptyReport()
+        {
+            DashboardContext context = CreateDbContext();
+            DiskBenchmark b = new DiskBenchmark("/test-empty-report");
+            var entity = new DiskBenchmarkEntity() { CreatedAt = DateTime.UtcNow, MountPath = b.Parameters.WorkFolder};
+            entity.Args = b.Parameters;
+            entity.Report = b.Prorgess;
+            context.DiskBenchmark.Add(entity);
+            context.SaveChanges();
+        }
+        
+        [Test]
+        public void Test_REAL()
+        {
+            DashboardContext context = CreateDbContext();
+            DiskBenchmark b = new DiskBenchmark(".", 128*1024,DataGeneratorFlavour.Random, 4096, 1);
+            b.Perform();
+            var entity = new DiskBenchmarkEntity() { CreatedAt = DateTime.UtcNow, MountPath = b.Parameters.WorkFolder};
+            entity.Args = b.Parameters;
+            entity.Report = b.Prorgess;
+            context.DiskBenchmark.Add(entity);
+            context.SaveChanges();
+            
+        }
+
+    }
+}
---
+++ Universe.Dashboard.DAL/Migrations/20190605081358_Add_DiskBenchmarkHistory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Universe.Dashboard.DAL.Migrations
+{
+    public partial class Add_DiskBenchmarkHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DiskBenchmark",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    MountPath = table.Column<string>(nullable: true),
+                    Args = table.Column<string>(nullable: true),
+                    Report = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiskBenchmark", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiskBenchmark");
+        }
+    }
+}
