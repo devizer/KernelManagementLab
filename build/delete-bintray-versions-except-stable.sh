6b8632e2730a7b8bc3d2c971007e9cfc60cd1fda
devizer <devizer@github.io>
2019-06-10 09:27:01 +0300

    the finish of the bintray storage N1

---
+++ build/delete-bintray-versions-except-stable.sh
@@ -0,0 +1,30 @@
+API=https://api.bintray.com
+BINTRAY_USER=devizer
+BINTRAY_REPO=W3-Top
+PCK_NAME=W3Top
+
+base_url="curl -u${BINTRAY_USER}:${BINTRAY_API_KEY} -H Content-Type:application/json -H Accept:application/json"
+url="$base_url -X GET ${API}/packages/${BINTRAY_USER}/${BINTRAY_REPO}/${PCK_NAME}"
+jsonPackage=`eval $url`
+printf "\n\nPACKAGE\n${jsonPackage}\n"
+jsonVersions=`echo $jsonPackage | jq -r '.versions'`
+printf "\n\nVERSIONS\n${jsonVersions}\n"
+
+jsonVersions2=`echo $jsonPackage | jq -r '.versions[]'`
+printf "\n\nVERSIONS2\n${jsonVersions2}\n"
+
+for ver in $jsonVersions2; do
+  echo checking version $ver ...
+  if [[ "${ver}" != "${VERSION_STABLE}" ]]; then
+    echo "delete version $ver"
+    delete_version "$ver"
+  else
+    echo Keep version $ver
+  fi
+done
+
+function delete_version() {
+  $v=$1
+  url="$base_url -X DELETE ${API}/packages/${BINTRAY_USER}/${BINTRAY_REPO}/${PCK_NAME}/versions/$v"
+  eval $url
+}
