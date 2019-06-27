34a5c1c4f3e63093255d8f1b1e15b512535443b6
devizer <devizer@github.io>
2019-06-27 20:39:58 +0300

    prepare integration tests

---
+++ Universe.Dashboard.DAL.Tests/prepare-tests.generated.sh
@@ -0,0 +1,30 @@
+
+# postgres
+PGSQL_TEST_SERVER_54321="Host=localhost;Port=54321;Database=postgres;Username=postgres;Password=pass;Timeout=15;"
+export PGSQL_TEST_SERVER_54321
+PGSQL_TEST_SERVER_54322="Host=localhost;Port=54322;Database=postgres;Username=postgres;Password=pass;Timeout=15;"
+export PGSQL_TEST_SERVER_54322
+PGSQL_TEST_SERVER_54323="Host=localhost;Port=54323;Database=postgres;Username=postgres;Password=pass;Timeout=15;"
+export PGSQL_TEST_SERVER_54323
+PGSQL_TEST_SERVER_54324="Host=localhost;Port=54324;Database=postgres;Username=postgres;Password=pass;Timeout=15;"
+export PGSQL_TEST_SERVER_54324
+PGSQL_TEST_SERVER_54325="Host=localhost;Port=54325;Database=postgres;Username=postgres;Password=pass;Timeout=15;"
+export PGSQL_TEST_SERVER_54325
+PGSQL_TEST_SERVER_54326="Host=localhost;Port=54326;Database=postgres;Username=postgres;Password=pass;Timeout=15;"
+export PGSQL_TEST_SERVER_54326
+PGSQL_TEST_SERVER_54327="Host=localhost;Port=54327;Database=postgres;Username=postgres;Password=pass;Timeout=15;"
+export PGSQL_TEST_SERVER_54327
+PGSQL_TEST_SERVER_54328="Host=localhost;Port=54328;Database=postgres;Username=postgres;Password=pass;Timeout=15;"
+export PGSQL_TEST_SERVER_54328
+
+# mysql
+MYSQL_TEST_SERVER_3307="Server=localhost;Database=mysql;Port=3307;Uid=root;Pwd=pass;Connect Timeout=15;Pooling=false;"
+export MYSQL_TEST_SERVER_3307
+MYSQL_TEST_SERVER_3308="Server=localhost;Database=mysql;Port=3308;Uid=root;Pwd=pass;Connect Timeout=15;Pooling=false;"
+export MYSQL_TEST_SERVER_3308
+MYSQL_TEST_SERVER_3309="Server=localhost;Database=mysql;Port=3309;Uid=root;Pwd=pass;Connect Timeout=15;Pooling=false;"
+export MYSQL_TEST_SERVER_3309
+MYSQL_TEST_SERVER_3310="Server=localhost;Database=mysql;Port=3310;Uid=root;Pwd=pass;Connect Timeout=15;Pooling=false;"
+export MYSQL_TEST_SERVER_3310
+MYSQL_TEST_SERVER_3311="Server=localhost;Database=mysql;Port=3311;Uid=root;Pwd=pass;Connect Timeout=15;Pooling=false;"
+export MYSQL_TEST_SERVER_3311
---
+++ Universe.Dashboard.DAL.Tests/prepare-tests.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+export MYSQL_TEST_DB=W3Top MYSQL_ROOT_PASSWORD=pass
+url=https://raw.githubusercontent.com/devizer/glist/master/install-5-mysqls-for-tests-V2.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -skSL $url) | bash
+
+export POSTGRESQL_DB=W3Top POSTGRESQL_PASS=pass POSTGRESQL_USER=postgres WAIT_TIMEOUT=13
+url=https://raw.githubusercontent.com/devizer/glist/master/install-7-postres-for-tests.sh; (wget -q -nv --no-check-certificate -O - $url 2>/dev/null || curl -skSL $url) | bash
+
+file=prepare-tests.generated.sh
+printf "" > $file
+
+echo '
+# postgres' >> $file
+for p in {54321..54328}; do
+  name="PGSQL_TEST_SERVER_$p"
+  line="$name=\"Host=localhost;Port=$p;Database=postgres;Username=postgres;Password=pass;Timeout=15;\""
+  echo "$line" >> $file
+  echo "export $name" >> $file
+done
+
+echo '
+# mysql' >> $file
+for p in {3307..3311}; do
+  name="MYSQL_TEST_SERVER_$p"
+  line="$name=\"Server=localhost;Database=mysql;Port=$p;Uid=root;Pwd=pass;Connect Timeout=15;Pooling=false;\""
+  echo "$line" >> $file
+  echo "export $name" >> $file
+done
+
+chmod +x $file
