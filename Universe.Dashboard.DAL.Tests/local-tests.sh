34c76a94b9b82b8ca4c65e007da3ef103e6cc89f
devizer <devizer@github.io>
2019-06-29 07:51:55 +0300

    travis error check for DAL.Tests 2

--- Universe.Dashboard.DAL.Tests/local-tests.sh
+++ Universe.Dashboard.DAL.Tests/local-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "MYSQL_TEST_CONNECTION: [$MYSQL_TEST_CONNECTION]"
-./prepare-db-servers.sh
+bash -e ./prepare-db-servers.sh
 source ./prepare-db-servers.generated.sh
 dotnet test -v:m -c Release
 
