0cde03eee16e24553f64c8ef528f2636f11fe9d0
devizer <devizer@github.io>
2019-07-28 03:10:35 +0300

    build colors

--- publish-public.sh
+++ publish-public.sh
@@ -4,7 +4,11 @@ set -e
 set -u
 
 export DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
-function header() { LightGreen='\033[1;32m';Yellow='\033[1;33m';RED='\033[0;31m'; NC='\033[0m'; printf "${LightGreen}$1${NC} ${Yellow}$2${NC}\n"; }
+function header() {
+  startAt=${startAt:-$(date +%s)}; elapsed=$(date +%s); elapsed=$((elapsed-startAt)); elapsed=$(TZ=UTC date -d "@${elapsed}" "+%_H:%M:%S");
+  LightGreen='\033[1;32m'; Yellow='\033[1;33m'; RED='\033[0;31m'; NC='\033[0m'; LightGray='\033[1;2m';
+  printf "${LightGray}${elapsed}${NC} ${LightGreen}$1${NC} ${Yellow}$2${NC}\n"; 
+}
 counter=0;
 function say() { counter=$((counter+1)); header "STEP $counter" "$1"; }
 
