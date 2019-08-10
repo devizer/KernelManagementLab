a5481964496af90f3417d1b376c6f6387162318f
devizer <devizer@github.io>
2019-08-11 00:39:09 +0300

    makeup

--- Universe.W3Top/ClientApp/src/components/DiskBenchmark/DiskBenchmarkHistory.js
+++ Universe.W3Top/ClientApp/src/components/DiskBenchmark/DiskBenchmarkHistory.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import MomentFormat from 'moment';
 
-import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import Paper from '@material-ui/core/Paper';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import Typography from '@material-ui/core/Typography';
-import Popper from '@material-ui/core/Popper';
 
-import Avatar from '@material-ui/core/Avatar';
-import Chip from '@material-ui/core/Chip';
-import FaceIcon from '@material-ui/icons/Face';
-import DoneIcon from '@material-ui/icons/Done';
 import {faCheck, faCheckDouble} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
