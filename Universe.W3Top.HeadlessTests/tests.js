3d3fe2cf12bd7af763c306c2dc142d60b45da5fd
devizer <devizer@github.io>
2019-08-18 13:16:19 +0300

    headless tests for travis

--- Universe.W3Top.HeadlessTests/tests.js
+++ Universe.W3Top.HeadlessTests/tests.js
@@ -10,29 +10,35 @@ const w3topUrl = process.env.W3TOP_URL || "http://localhost:5050/mounts";
 
 (async function() {
 
-  async function launchChrome() {
-    return await chromeLauncher.launch({
-      // chromePath: 'google-chrome',
-      startingUrl: 'about:blank',
-      chromeFlags: ['--headless', '--disable-gpu', "--no-sandbox", "--enable-logging"],
-    });
-  }
+    async function launchChrome() {
+        return await chromeLauncher.launch({
+            // chromePath: 'google-chrome',
+            startingUrl: 'about:blank',
+            chromeFlags: ['--headless', '--disable-gpu', "--no-sandbox", "--enable-logging"],
+        });
+    }
 
-  const chrome = await launchChrome();
-  console.log(`Chrome port: ${chrome.port}`);
-  
-  const protocol = await CDP({port: chrome.port});
+    const chrome = await launchChrome();
+    console.log(`Chrome port: ${chrome.port}`);
 
-  const { DOM, Page, Emulation, Runtime} = protocol;
-  
-  const getExpression = async (expression) => {
-      const innerText = await Runtime.evaluate({expression: expression});
-      return innerText.result.value;
-  };
-  
-  const getElementById = async (idName) => {
-      return await getExpression(`document.getElementById('${idName}').innerText`);
-  };
+    const protocol = await CDP({port: chrome.port});
+
+    const { DOM, Page, Emulation, Runtime} = protocol;
+
+    const getExpression = async (expression) => {
+        const expressionValue = await Runtime.evaluate({expression: expression});
+        return expressionValue.result.value;
+    };
+
+    const getElementById = async (idName) => {
+        return await getExpression(`document.getElementById('${idName}').innerText`);
+    };
+    
+    const getVersion = async () => {
+        const userAgent = await getExpression("navigator.userAgent");
+        const raw = userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+        return raw ? parseInt(raw[2], 10) : false;
+    };
 
     const waitForTrigger = async (timeout, triggerKey, isSuccess) => {
         const start = new Date();
@@ -52,56 +58,60 @@ const w3topUrl = process.env.W3TOP_URL || "http://localhost:5050/mounts";
 
         console.warn(`Warning! trigger [${triggerKey}] was not raised in ${new Date() - start} milliseconds`);
         return false;
-  };
-  
-  await Promise.all([Page.enable(), Runtime.enable(), DOM.enable()]);
+    };
 
-  Page.navigate({ url: w3topUrl });
+    await Promise.all([Page.enable(), Runtime.enable(), DOM.enable()]);
 
-  Page.loadEventFired(async() => {
-    console.log("PAGE LOADED");
-    console.log(`TITLE: '${await getExpression("document.title")}'`);
+    Page.navigate({ url: w3topUrl });
 
-/*
-      console.log(`WHOLE DOCUMENT (incomplete): 
-${await getExpression("document.body.innerText")}
-`);
-*/
-      
-    for(let sysInfoIndex=1; sysInfoIndex<=4; sysInfoIndex++)
-    {
-      let id=`FOOTER_INFO_HEADER_${sysInfoIndex}`;
-      const headerValue = await getElementById(id);
-      if (headerValue === undefined)
-        console.error(`ERROR: Missed Footer Info Header ${id}`);
-      else
-        console.log(`Header [${id}]: '${headerValue ? headerValue : "MISSED"}'`);
-    }
-
-      let areMetricsArriving = await waitForTrigger(15000,"MetricsArriving", status => status === "true");
-      if (!areMetricsArriving)
-          console.error("ERROR: Web Socket broadcast for metrics is not obtained in 15 seconds");
-
-      let areMetricsArrived = await waitForTrigger(5000,"MetricsArrived", status => status === "true");
-      if (!areMetricsArrived)
-          console.error("ERROR: Metrics are not bound in 5 seconds");
-
-/*
-      console.log(`WHOLE COMPLETED DOCUMENT: 
-${await getExpression("document.body.innerText")}
-`);
-*/
+    Page.loadEventFired(async() => {
+        console.log("PAGE LOADED");
+        console.log(`TITLE: '${await getExpression("document.title")}'`);
+        console.log(`Visibility State: '${await getExpression("document.visibilityState")}'`);
+        console.log(`User Agent: '${await getExpression("navigator.userAgent")}'`);
+        console.log(`Version: '${await getVersion()}'`);
 
 
-      const ss = await Page.captureScreenshot({format: 'png', fromSurface: true});
-      file.writeFile('bin/screenshot [home].png', ss.data, 'base64', function(err) {
-          if (err) console.log(`Screenshot error: ${err}`);
-      });
+        /*
+              console.log(`WHOLE DOCUMENT (incomplete): 
+        ${await getExpression("document.body.innerText")}
+        `);
+        */
 
-    protocol.close();
-    chrome.kill();
-    console.log("The End");
-  });
+        for(let footerHeaderIndex=1; footerHeaderIndex<=4; footerHeaderIndex++)
+        {
+            let id=`FOOTER_INFO_HEADER_${footerHeaderIndex}`;
+            const headerValue = await getElementById(id);
+            if (headerValue === undefined)
+                console.error(`ERROR: Missed Footer Info Header ${id}`);
+            else
+                console.log(`Header [${id}]: '${headerValue ? headerValue : "MISSED"}'`);
+        }
 
-})();
+        let areMetricsArriving = await waitForTrigger(15000,"MetricsArriving", status => status === "true");
+        if (!areMetricsArriving)
+            console.error("ERROR: Web Socket broadcast for metrics is not obtained in 15 seconds");
+
+        let areMetricsArrived = await waitForTrigger(5000,"MetricsArrived", status => status === "true");
+        if (!areMetricsArrived)
+            console.error("ERROR: Metrics are not bound in 5 seconds");
+
+        /*
+              console.log(`WHOLE COMPLETED DOCUMENT: 
+        ${await getExpression("document.body.innerText")}
+        `);
+        */
+
+
+        const ss = await Page.captureScreenshot({format: 'png', fromSurface: true});
+        file.writeFile('bin/screenshot [home].png', ss.data, 'base64', function(err) {
+            if (err) console.log(`Screenshot error: ${err}`);
+        });
+
+        protocol.close();
+        chrome.kill();
+        console.log("The End");
+    });
+
+})().then(() => {});
 
