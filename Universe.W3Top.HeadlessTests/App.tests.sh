2588ceae9173080a08a9702a5b0b55af4a6ef87a
devizer <devizer@github.io>
2019-08-20 19:22:14 +0300

    benchmark history for screenshot

--- Universe.W3Top.HeadlessTests/App.tests.sh
+++ Universe.W3Top.HeadlessTests/App.tests.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
+
+info='to prepare data for disk benchmark history on travis-ci:
+http://localhost:5050/api/benchmark/disk/start-disk-benchmark
+
+ccept: application/json
+payload: {workingSet: "128", randomAccessDuration: "2", disableODirect: false, blockSize: 4096, threads: 16}
+
+ 
+curl -i -H "Accept: application/json" \
+    -X POST -d '{workingSet: "128", randomAccessDuration: "2", disableODirect: false, blockSize: 4096, threads: 16}' \
+    http://localhost:5050/api/benchmark/disk/start-disk-benchmark
+'
+
 yarn test
 
