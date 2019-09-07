4a4b7713427c8e85ce0de12359897d381ffa97e1
devizer <devizer@github.io>
2019-09-07 12:08:16 +0300

    PreJIT 4 routes using Universe.HttpWaiter V6 (for slow arm)

--- Universe.W3Top/Startup.cs
+++ Universe.W3Top/Startup.cs
@@ -177,6 +177,7 @@ namespace Universe.W3Top
         {
             Console.WriteLine($"Waiting for {server}:{port}");
             Stopwatch sw = Stopwatch.StartNew();
+            Exception error = null;
             do
             {
                 try
@@ -186,18 +187,19 @@ namespace Universe.W3Top
                     {
                         client.Connect(server, port);
                         Console.WriteLine(
-                            $"TCP connection to {server}:{port} is available in {sw.ElapsedMilliseconds:n0} milliseconds");
+                            $"TCP connection to {server}:{port} established in {sw.ElapsedMilliseconds:n0} milliseconds");
                         return true;
                     }
                 }
-                catch
+                catch(Exception ex)
                 {
+                    error = ex;
                     Thread.Sleep(42);
                 }
 
             } while (sw.ElapsedMilliseconds <= timeout * 1000);
             
-            Console.WriteLine($"TCP connection to {server}:{port} is NOT available during {sw.ElapsedMilliseconds:n0} milliseconds");
+            Console.WriteLine($"Warning! TCP connection to {server}:{port} is NOT available during {sw.ElapsedMilliseconds:n0} milliseconds. {error.GetExceptionDigest()}");
             return false;
 
         }
