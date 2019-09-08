9ad2b0161b6d914769cae7051d584bd8d1b5b9ce
devizer <devizer@github.io>
2019-09-08 05:28:55 +0300

    PreJIT 4 routes using Universe.HttpWaiter V9 (for slow arm)

--- Universe.W3Top/Startup.AspNetJit.cs
+++ Universe.W3Top/Startup.AspNetJit.cs
@@ -17,20 +17,25 @@ namespace Universe.W3Top
 
             string[] reqs =
             {
-                $"{urlBase}/; Method=Get; Valid Status = 100-299",
-                $"{urlBase}/manifest.json; Method=Get; Valid Status = 100-299",
-                $"{urlBase}/api/BriefInfo; Method=Get; Valid Status = 100-299",
-                $"{urlBase}/api/benchmark/disk/get-disks; Method=Get; Valid Status = 100-299",
+                $"{urlBase}/; Method=Get; Valid Status = 100-399",
+                $"{urlBase}/manifest.json; Method=Get; Valid Status = 100-399",
+                $"{urlBase}/api/BriefInfo; Method=Get; Valid Status = 100-399",
+                $"{urlBase}/api/benchmark/disk/get-disks; Method=Get; Valid Status = 100-399",
             };
 
             Parallel.ForEach(reqs, (req) =>
             {
                 try
                 {
-                    Stopwatch startAt = Stopwatch.StartNew();
-                    HttpConnectionString cs = new HttpConnectionString(req);
-                    HttpProbe.Go(cs).Wait();
-                    Console.WriteLine($"Pre-JITed [{req}] in {startAt.Elapsed}");
+                    double[] durations = new double[2]; 
+                    for (int i = 0; i <= 1; i++)
+                    {
+                        Stopwatch startAt = Stopwatch.StartNew();
+                        HttpConnectionString cs = new HttpConnectionString(req);
+                        HttpProbe.Go(cs).Wait();
+                        durations[i] = startAt.ElapsedTicks * 1000d / Stopwatch.Frequency;
+                    }
+                    Console.WriteLine($"Pre-JITed [{req}]. First: {durations[0]:n1}, Next: {durations[1]:n1} msecs");
                 }
                 catch(Exception ex)
                 {
