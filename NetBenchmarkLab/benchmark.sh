fce2b47efa7f2f53fe897ca226393b8f6f82a421
devizer <devizer@github.io>
2019-09-10 07:08:08 +0300

    makeup

--- NetBenchmarkLab/benchmark.sh
+++ NetBenchmarkLab/benchmark.sh
@@ -5,9 +5,9 @@ export DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 
 cd ~
-rm -rf net || true
-git clone https://github.com/devizer/KernelManagementLab net
-cd net/NetBenchmarkLab
+rm -rf net.benchmark || true
+git clone https://github.com/devizer/KernelManagementLab net.benchmark
+cd net.benchmark/NetBenchmarkLab
 dotnet build -c Release -o bin/temp
 cd bin/temp
 time dotnet NetBenchmarkLab.dll | tee NetBenchmarkLab.report.log 
