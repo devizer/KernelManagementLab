650b749d33416919b8724b7d89ffc04c02603f82
devizer <devizer@github.io>
2019-09-19 20:55:44 +0300

    Latency-Sorted-Report.txt

--- NetBenchmarkLab/TestSpeed1.cs
+++ NetBenchmarkLab/TestSpeed1.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using KernelManagementJam;
 using NetBenchmarkLab.NetBenchmarkModel;
 using SpeedTest;
@@ -23,30 +27,57 @@ namespace NetBenchmarkLab
             var servers = settings.Servers.Take(9999).ToArray();
             int len = servers.Select(x => GetTitle(x).Length).Max();
             // Console.WriteLine($"Server: {server.Name}");
+            List<ServerLatency> latencyLog = new List<ServerLatency>();
             for (int i = 0; i < servers.Length; i++)
             {
                 Stopwatch swError = Stopwatch.StartNew();
-                Console.Write($"{i,2}: {{0,{len}}}", GetTitle(servers[i]));
+                StringBuilder reportRow = new StringBuilder(string.Format($"{i,2}: {{0,{len}}}", GetTitle(servers[i])));
+                Console.Write(reportRow);
                 try
                 {
+                    double latency = 42*42*42*42d;
                     for (int t = 0; t < 5; t++)
                     {
                         Stopwatch sw = Stopwatch.StartNew();
-                        var latency = client.TestServerLatency(servers[i], retryCount: 1);
+                        var latency1 = client.TestServerLatency(servers[i], retryCount: 1);
                         var latency2 = client.TestServerCorrectLatency(servers[i], 1);
-                        Console.Write($" {latency.ToString("f0")+"/" + latency2.ToString("f2"),13}");
-
-                        
+                        var column = $" {latency1.ToString("f0")+"/" + latency2.ToString("f2"),13}";
+                        reportRow.Append(column);
+                        Console.Write(column);
+                        latency = Math.Min(latency, latency2);
                     }
+
+                    latencyLog.Add(new ServerLatency
+                    {
+                        LogLine = reportRow.ToString(),
+                        Latency = latency,
+                        Server = servers[i]
+                    });
+                    
                 }
                 catch (Exception ex)
                 {
-                    Console.Write(ex.GetExceptionDigest() + " " + swError.Elapsed);
+                    Console.Write(" " + ex.GetExceptionDigest() + " " + swError.Elapsed);
                 }
                 Console.WriteLine();
+
+                if (i % 12 == 0 || i == servers.Length - 1)
+                {
+                    var sorted = latencyLog.OrderBy(x => x.Latency).ToArray();
+                    File.WriteAllText("Latency-Sorted-Report.txt", string.Join(Environment.NewLine, sorted.Select(x => x.LogLine)));
+                }
             }
 
         }
+
+
+        class ServerLatency
+        {
+            public Server Server;
+            public String LogLine;
+            public double Latency;
+        }
+        
 
     }
 }
