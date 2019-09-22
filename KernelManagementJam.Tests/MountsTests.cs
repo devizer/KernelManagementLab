9f1f75d213a7ee8c59d39b3f78ddb3564c0b753e
devizer <devizer@github.io>
2019-09-22 16:11:47 +0300

    Mounts Tests

---
+++ KernelManagementJam.Tests/MountsTests.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using KernelManagementJam.DebugUtils;
+using NUnit.Framework;
+
+namespace KernelManagementJam.Tests
+{
+    [TestFixture]
+    public class MountsTests
+    {
+        [Test]
+        public void Root_FileSystem_Exists()
+        {
+            bool isLinux = Environment.OSVersion.Platform == PlatformID.Unix;
+            if (!isLinux)
+            {
+                Console.WriteLine("Warning! Test Ignored. Linux is required");
+                return;
+            }
+            
+            
+            IList<MountEntry> mounts = ProcMountsParser.Parse("/proc/mounts").Entries;
+            var root1 = mounts.FirstOrDefault(x => x.MountPath == "/");
+            Assert.IsNotNull(root1, "ProcMountsParser.Parse: MountPath == '/' exists");
+
+            ProcMountsAnalyzer analyz = ProcMountsAnalyzer.Create(mounts, skipDetailsLog: true);
+            var root2 = analyz.Details.FirstOrDefault(x => x.MountEntry.MountPath == "/");
+            Assert.IsNotNull(root2, "ProcMountsAnalyzer.Create: MountPath == '/' exists");
+            
+            Console.WriteLine($"root volume:{Environment.NewLine}{root2.AsJson()}");
+            
+            Assert.Greater(root2.FreeSpace, 0, "FreeSpace of the '/' is positive number");
+            Assert.Greater(root2.TotalSize, 0, "TotalSize of the '/' is positive number");
+            
+            Assert.IsNotNull(root2.MountEntry?.Device, "MountEntry.Device of the '/' is not null");
+            Assert.IsNotEmpty(root2.MountEntry?.Device, "MountEntry.Device of the '/' is not empty");
+            
+            Assert.IsNotNull(root2.MountEntry?.FileSystem, "MountEntry.FileSystem of the '/' is not null");
+            Assert.IsNotEmpty(root2.MountEntry?.FileSystem, "MountEntry.FileSystem of the '/' is not empty");
+            
+            
+            
+            // analyz.Details;
+            return;
+        }
+        
+    }
+}
