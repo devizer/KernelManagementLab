f783ddda2490f41a9c33f5fc390c0129d2482c53
devizer <devizer@users.github.com>
2019-10-17 12:22:54 +0300

    test-linux-resource-usage.sh

---
+++ KernelManagementJam.Tests/test-linux-resource-usage.sh
@@ -0,0 +1,8 @@
+script=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-and-nodejs.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash -s dotnet
+cd ~
+rm -rf KernelManagementLab || true
+git clone https://github.com/devizer/KernelManagementLab
+cd KernelManagementLab/KernelManagementJam.Tests
+
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+git pull; time dotnet test --filter LinuxResourcesUsage
