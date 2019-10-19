8e03d0f3946b7e0e9823f6a9162e7c9bf76ed6be
devizer <devizer@github.io>
2019-10-19 09:40:01 +0300

    thread_info for Mac OS

--- KernelManagementJam.Tests/MacOsResourcesUsage_Tests.cs
+++ KernelManagementJam.Tests/MacOsResourcesUsage_Tests.cs
@@ -9,11 +9,31 @@ namespace KernelManagementJam.Tests
     [TestFixture]
     public class MacOsResourcesUsage_Tests : NUnitTestsBase
     {
+        private static bool IsMacOs => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+        
         [Test]
-        public void mach_thread_self_Test1()
+        public void _1_mach_thread_self_Test1()
         {
+            if (!IsMacOs) return;
+            
             int threadId = MacOsThreadInfoInterop.mach_thread_self();
             Console.WriteLine($"Thread ID: {threadId}");
+            int resDe = MacOsThreadInfoInterop.mach_port_deallocate(MacOsThreadInfoInterop.mach_thread_self(), threadId);
+            Console.WriteLine($"mach_port_deallocate result: {resDe}");
+        }
+
+        [Test]
+        public void _2_thread_info_Test1()
+        {
+            if (!IsMacOs) return;
+
+            int threadId = MacOsThreadInfoInterop.mach_thread_self();
+            Console.WriteLine($"Thread ID: {threadId}");
+
+            var raw = MacOsThreadInfoInterop.GetRawThreadInfo(threadId);
+            for(int i=0; i<raw.Length; i++)
+                Console.WriteLine($"  {i}: {raw[i]}");
+            
             int resDe = MacOsThreadInfoInterop.mach_port_deallocate(MacOsThreadInfoInterop.mach_thread_self(), threadId);
             Console.WriteLine($"mach_port_deallocate result: {resDe}");
         }
@@ -27,6 +47,25 @@ namespace KernelManagementJam.Tests
         // mach_port_deallocate
         [DllImport("libc", SetLastError = false, EntryPoint = "mach_port_deallocate")]
         public static extern int mach_port_deallocate(int threadId, int materializedThreadId);
+        
+        [DllImport("libc", SetLastError = false)]
+        public static extern int thread_info(int threadId, int flavor, ref ThreadInfo info, ref int count);
+
+        public class ThreadInfo
+        {
+            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.I4, SizeConst = 10)]
+            public int[] Raw;
+        }
+
+        public static int[] GetRawThreadInfo(int threadId)
+        {
+            ThreadInfo info = new ThreadInfo() {Raw = new int[10]};
+            int count = 40;
+            int result = thread_info(threadId, 3, ref info, ref count);
+            Console.WriteLine($"thread_info return value:${result}");
+            return info.Raw;
+        }
+
         
     }
 }
