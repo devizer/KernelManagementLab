ab7b6dd20045f6ad6b880305f16ccae406993a59
devizer <devizer@github.io>
2019-10-19 10:37:59 +0300

    osx thread info 4

--- KernelManagementJam.Tests/MacOsResourcesUsage_Tests.cs
+++ KernelManagementJam.Tests/MacOsResourcesUsage_Tests.cs
@@ -37,6 +37,22 @@ namespace KernelManagementJam.Tests
             int resDe = MacOsThreadInfoInterop.mach_port_deallocate(MacOsThreadInfoInterop.mach_thread_self(), threadId);
             Console.WriteLine($"mach_port_deallocate result: {resDe}");
         }
+
+        [Test]
+        public void _3_thread_info_Test2_Custom()
+        {
+            if (!IsMacOs) return;
+
+            int threadId = MacOsThreadInfoInterop.mach_thread_self();
+            Console.WriteLine($"Thread ID: {threadId}");
+
+            var raw = MacOsThreadInfoInterop.GetRawThreadInfo(threadId);
+            for(int i=0; i<raw.Length; i++)
+                Console.WriteLine($"  {i}: {raw[i]}");
+            
+            int resDe = MacOsThreadInfoInterop.mach_port_deallocate(MacOsThreadInfoInterop.mach_thread_self(), threadId);
+            Console.WriteLine($"mach_port_deallocate result: {resDe}");
+        }
     }
 
     class MacOsThreadInfoInterop
@@ -48,9 +64,9 @@ namespace KernelManagementJam.Tests
         [DllImport("libc", SetLastError = false, EntryPoint = "mach_port_deallocate")]
         public static extern int mach_port_deallocate(int threadId, int materializedThreadId);
         
-        [DllImport("libc", SetLastError = false, CharSet=CharSet.Ansi,
-            CallingConvention=CallingConvention.Cdecl)]
+        [DllImport("libc", SetLastError = false)]
         public static extern int thread_info(int threadId, int flavor, ref ThreadInfo info, int count);
+        
 
         public class ThreadInfo
         {
@@ -65,6 +81,27 @@ namespace KernelManagementJam.Tests
             int result = thread_info(threadId, 3, ref info, count);
             Console.WriteLine($"thread_info return value:${result}");
             return info.Raw;
+        }
+
+        [DllImport("libc", SetLastError = false, EntryPoint = "thread_info")]
+        public static extern int thread_info_custom(int threadId, int flavor, IntPtr threadInfo, int count);
+        public static unsafe int[] GetRawThreadInfo_Custom(int threadId)
+        {
+            IntPtr threadInfo = Marshal.AllocHGlobal(40);
+            int count = 40;
+            int result = thread_info_custom(threadId, 3, threadInfo, count);
+            Console.WriteLine($"thread_info return value:${result}");
+
+            int[] ret = new int[10];
+            int* ptr = (int*) threadInfo.ToPointer();
+            for (int i = 0; i < 10; i++)
+            {
+                ret[i] = *ptr;
+                ptr++;
+            }
+            
+            Marshal.FreeHGlobal(threadInfo);
+            return ret;
         }
 
         
