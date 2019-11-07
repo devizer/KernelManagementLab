a6191aa0a18a03033adb2ca96ddd09c6e24e3826
devizer <devizer@users.github.com>
2019-11-07 05:59:47 +0200

    Added AdvancedStopwatch is not a source

---
+++ KernelManagementJam/ThreadInfo/AdvancedStopwatch.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Resources;
+using System.Text;
+
+namespace KernelManagementJam.ThreadInfo
+{
+    public class AdvancedStopwatch
+    {
+        public static readonly long Frequency;
+        public static readonly bool IsHighResolution;
+
+        private CpuUsage startedCpuUsage;
+        private Stopwatch startedDuration;
+        private bool isRunning = false;
+
+        private CpuUsage elapsedCpuUsage;
+
+        public bool IsRunning
+        {
+            get { return isRunning; }
+        }
+
+        static AdvancedStopwatch()
+        {
+            Frequency = Stopwatch.Frequency;
+            IsHighResolution = Stopwatch.IsHighResolution;
+        }
+
+        public AdvancedStopwatch()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            elapsedCpuUsage = new CpuUsage();
+            elapsedTicks = 0;
+            isRunning = false;
+            startedDuration.Reset();
+            startedCpuUsage = new CpuUsage();
+        }
+
+        public void Restart()
+        {
+            startedDuration.Restart();
+            elapsedTicks = 0;
+            elapsedCpuUsage = new CpuUsage();
+            startedCpuUsage = CpuUsageReader.GetByThread() ?? new CpuUsage();
+            isRunning = true;
+        }
+
+        public TimeSpan Elapsed
+        {
+            get { return startedDuration.Elapsed; }
+        }
+
+
+
+    }
+}
