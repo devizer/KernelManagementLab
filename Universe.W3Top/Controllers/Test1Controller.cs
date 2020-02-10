0f3a5b47742dc267bf878b9f35bd4cc9d4257506
devizer <devizer@github.io>
2020-02-10 18:29:30 +0200

    inlined clarification

--- Universe.W3Top/Controllers/Test1Controller.cs
+++ Universe.W3Top/Controllers/Test1Controller.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using KernelManagementJam;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Universe.W3Top.Controllers
 {
     [Route("api/[controller]")]
+    [Obsolete("wipe it off", error:false)]
     public class Test1Controller : ControllerBase
     {
         [HttpGet("[action]")]
