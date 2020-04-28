4711752a03a285bb53d334d06aa383dc73b8ffdc
devizer <devizer@github.io>
2020-04-28 10:03:09 +0300

    fixed postres dbs for tests config again

--- Universe.Dashboard.DAL.Tests/prepare-db-servers.generated.sh
+++ Universe.Dashboard.DAL.Tests/prepare-db-servers.generated.sh
@@ -8,21 +8,21 @@ export MSSQL_TEST_SERVER_2019
 
 
 # postgres
-PGSQL_TEST_SERVER_54321="Host=localhost;Port=54321;Database=postgres;Username=postgres;Password=pass;Timeout=15;Pooling=false;"
+PGSQL_TEST_SERVER_54321="Host=localhost;Port=54321;Database=postgres;Username=postgres;Password=passw0rd;Timeout=15;Pooling=false;"
 export PGSQL_TEST_SERVER_54321
-PGSQL_TEST_SERVER_54322="Host=localhost;Port=54322;Database=postgres;Username=postgres;Password=pass;Timeout=15;Pooling=false;"
+PGSQL_TEST_SERVER_54322="Host=localhost;Port=54322;Database=postgres;Username=postgres;Password=passw0rd;Timeout=15;Pooling=false;"
 export PGSQL_TEST_SERVER_54322
-PGSQL_TEST_SERVER_54323="Host=localhost;Port=54323;Database=postgres;Username=postgres;Password=pass;Timeout=15;Pooling=false;"
+PGSQL_TEST_SERVER_54323="Host=localhost;Port=54323;Database=postgres;Username=postgres;Password=passw0rd;Timeout=15;Pooling=false;"
 export PGSQL_TEST_SERVER_54323
-PGSQL_TEST_SERVER_54324="Host=localhost;Port=54324;Database=postgres;Username=postgres;Password=pass;Timeout=15;Pooling=false;"
+PGSQL_TEST_SERVER_54324="Host=localhost;Port=54324;Database=postgres;Username=postgres;Password=passw0rd;Timeout=15;Pooling=false;"
 export PGSQL_TEST_SERVER_54324
-PGSQL_TEST_SERVER_54325="Host=localhost;Port=54325;Database=postgres;Username=postgres;Password=pass;Timeout=15;Pooling=false;"
+PGSQL_TEST_SERVER_54325="Host=localhost;Port=54325;Database=postgres;Username=postgres;Password=passw0rd;Timeout=15;Pooling=false;"
 export PGSQL_TEST_SERVER_54325
-PGSQL_TEST_SERVER_54326="Host=localhost;Port=54326;Database=postgres;Username=postgres;Password=pass;Timeout=15;Pooling=false;"
+PGSQL_TEST_SERVER_54326="Host=localhost;Port=54326;Database=postgres;Username=postgres;Password=passw0rd;Timeout=15;Pooling=false;"
 export PGSQL_TEST_SERVER_54326
-PGSQL_TEST_SERVER_54327="Host=localhost;Port=54327;Database=postgres;Username=postgres;Password=pass;Timeout=15;Pooling=false;"
+PGSQL_TEST_SERVER_54327="Host=localhost;Port=54327;Database=postgres;Username=postgres;Password=passw0rd;Timeout=15;Pooling=false;"
 export PGSQL_TEST_SERVER_54327
-PGSQL_TEST_SERVER_54328="Host=localhost;Port=54328;Database=postgres;Username=postgres;Password=pass;Timeout=15;Pooling=false;"
+PGSQL_TEST_SERVER_54328="Host=localhost;Port=54328;Database=postgres;Username=postgres;Password=passw0rd;Timeout=15;Pooling=false;"
 export PGSQL_TEST_SERVER_54328
 
 # mysql
