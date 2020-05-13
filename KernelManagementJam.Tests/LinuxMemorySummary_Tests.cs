b99e5b155f25c6de4c9e1f89c2340cda25bd668e
devizer <devizer@users.github.io>
2020-05-13 22:17:06 +0300

    LinuxMemorySummary_Tests.cs on windows/osx

--- KernelManagementJam.Tests/LinuxMemorySummary_Tests.cs
+++ KernelManagementJam.Tests/LinuxMemorySummary_Tests.cs
@@ -1,5 +1,6 @@
 using System;
 using NUnit.Framework;
+using Universe;
 
 namespace KernelManagementJam.Tests
 {
@@ -10,6 +11,8 @@ namespace KernelManagementJam.Tests
         [Test]
         public void Does_Work()
         {
+            if (CrossInfo.ThePlatform != CrossInfo.Platform.Linux) return;
+            
             if (!LinuxMemorySummary.TryParse(out var info))
                 Assert.Fail("LinuxMemorySummary.TryParse is not supported");
             
