308ae3baabaeb99157c390ed06d7cde0aca92204
devizer <devizer@github.io>
2020-05-21 22:28:04 +0300

    ProcessList

--- Universe.W3Top/ClientApp/src/components/ProcessList/ColumnChooserComponent.js
+++ Universe.W3Top/ClientApp/src/components/ProcessList/ColumnChooserComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import processListStore from "./Store/ProcessListStore";
+import DialogContent from "@material-ui/core/DialogContent";
 
 export class ColumnChooserComponent extends Component {
     static displayName = ColumnChooserComponent.name;
@@ -15,7 +16,28 @@ export class ColumnChooserComponent extends Component {
     render () {
         return (
             <div>
-                TODO: ColumnChooserComponent
+                <p className="cs-group">Process:</p>
+                <p className="cs-line">[X] PID, [X] Name, [X] Priority, [X] Uptime, [X] Command line</p>
+
+                <p className="cs-group">IO Time:</p>
+                <p className="cs-line">[X] Total, [X] Current</p>
+
+                <p className="cs-group">IO Transfer:</p>
+                <p className="cs-line">Logical Read: [X] Total, [X] Current. Logical Write [X] Total, [X] Current</p>
+                <p className="cs-line">Calls Read: [X] Total, [X] Current. Calls Write: [X] Total, [X] Current</p>
+                <p className="cs-line">Block Level Read: [X] Total, [X] Current. Block Level Write: [X] Total, [X] Current</p>
+
+                <p className="cs-group">Memory:</p>
+                <p className="cs-line">[X] RSS, [X] Shared, [X] Swapped</p>
+
+                <p className="cs-group">Page Faults:</p>
+                <p className="cs-line">Minor: [X] Total, [X] Current. Swapin: [X] Total, [X] Current</p>
+                <p className="cs-line">Children Minor: [X] Total, [X] Current. Children Swapins: [X] Total, [X] Current</p>
+
+                <p className="cs-group">CPU Usage:</p>
+                <p className="cs-line">User: [X] Total, [X] Current. Kernel: [X] Total, [X] Current. Total: [X] Total, [X] Current</p>
+                <p className="cs-line">Children User: [X] Total, [X] Current. Children Kernel: [X] Total, [X] Current. Children Total: [X] Total, [X] Current</p>
+
             </div>
         );
     }
