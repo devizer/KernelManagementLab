db9dc8471335e5838abafc1748d77f8aff7687e5
devizer <devizer@github.io>
2020-05-21 15:09:31 +0300

    fix layout of BlockStatChartHeader.js for Disk Live Chart again2

--- Universe.W3Top/ClientApp/src/components/ProcessList/ProcessListContainerV1.js
+++ Universe.W3Top/ClientApp/src/components/ProcessList/ProcessListContainerV1.js
@@ -70,13 +70,13 @@ export class ProcessListContainerV1 extends Component {
                     </DialogTitle>
                     <DialogContent style={{textAlign_ignore: "center"}} id="columns-chooser-content">
 
-                        <p className="cs-group">Process</p>                                        
+                        <p className="cs-group">Process:</p>                                        
                         <p className="cs-line">[X] PID, [X] Name, [X] Priority, [X] Uptime, [X] Command line</p>
 
-                        <p className="cs-group">IO Time</p>
+                        <p className="cs-group">IO Time:</p>
                         <p className="cs-line">[X] Total, [X] Current</p>
 
-                        <p className="cs-group">IO Transfer</p>
+                        <p className="cs-group">IO Transfer:</p>
                         <p className="cs-line">Logical Read: [X] Total, [X] Current. Logical Write [X] Total, [X] Current</p>
                         <p className="cs-line">Calls Read: [X] Total, [X] Current. Calls Write: [X] Total, [X] Current</p>
                         <p className="cs-line">Block Level Read: [X] Total, [X] Current. Block Level Write: [X] Total, [X] Current</p>
@@ -84,7 +84,7 @@ export class ProcessListContainerV1 extends Component {
                         <p className="cs-group">Memory:</p>
                         <p className="cs-line">[X] RSS, [X] Shared, [X] Swapped</p>
 
-                        <p className="cs-group">Page Faults</p>
+                        <p className="cs-group">Page Faults:</p>
                         <p className="cs-line">Minor: [X] Total, [X] Current. Swapin: [X] Total, [X] Current</p>
                         <p className="cs-line">Children Minor: [X] Total, [X] Current. Children Swapins: [X] Total, [X] Current</p>
 
