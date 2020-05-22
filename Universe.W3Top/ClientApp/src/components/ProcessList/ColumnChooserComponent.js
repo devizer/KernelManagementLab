e0176307657a06bf7ead083b36b0ef98241339db
devizer <devizer@github.io>
2020-05-22 03:14:14 +0300

    ProcessList: layout is done for ColumnChooserComponent.js

--- Universe.W3Top/ClientApp/src/components/ProcessList/ColumnChooserComponent.js
+++ Universe.W3Top/ClientApp/src/components/ProcessList/ColumnChooserComponent.js
@@ -1,6 +1,64 @@
 import React, {Component} from 'react';
-import processListStore from "./Store/ProcessListStore";
+import { withStyles } from '@material-ui/core/styles';
 import DialogContent from "@material-ui/core/DialogContent";
+
+import processListStore from "./Store/ProcessListStore";
+import Switch from '@material-ui/core/Switch';
+import {Checkbox,FormControlLabel,FormControl} from '@material-ui/core';
+
+
+function CustomCheckbox(props) {
+    return (
+        <Checkbox {...props}/>
+    );
+}
+
+class Welcome extends React.Component {
+    render() {
+        return <h1>Hello, {this.props.name}</h1>;
+    }
+}
+
+class FixedSpan extends React.Component {
+    render() {
+        return (<span style={{display: "inline-block", width: this.props.width, minWidth: this.props.width}}>{this.props.children}</span>);
+    }
+}
+
+class IoTransferLayout extends React.Component {
+    render() {
+        let x = this.props.x, y = this.props.y
+        let widths = [115, 95, 115, 45, 120, 95, 115];
+        let height = 40;
+        let left = 0; for (let c = 0; c < x; c++) left += widths[c];
+        let top = y * height - 7;
+        return (
+            <div style={{position: "absolute", left, top, whiteSpace:"nowrap"}}>
+                {this.props.children}
+            </div>
+        );
+    }
+};
+
+class CpUsageLayout extends React.Component {
+    render() {
+        let x = this.props.x, y = this.props.y;
+        let widths = [302, 302, 302];
+        let height = 40;
+        let left = 0; for (let c = 0; c < x; c++) left += widths[c];
+        let top = y * height - 7;
+        return (
+            <div style={{position: "absolute", left, top, whiteSpace:"nowrap"}}>
+                {this.props.children}
+            </div>
+        );
+    }
+};
+
+let ABS=IoTransferLayout;
+let ABS3=CpUsageLayout;
+
+
 
 export class ColumnChooserComponent extends Component {
     static displayName = ColumnChooserComponent.name;
@@ -14,31 +72,166 @@ export class ColumnChooserComponent extends Component {
     }
 
     render () {
+        let nowrap={whiteSpace:"nowrap"};
+        let space=(<>&nbsp;&nbsp;&nbsp;</>);
+        let chbox= (caption) => (
+            <><span style={nowrap}><CustomCheckbox color="primary" onChange={() => {}} value={"PID"} />{caption}</span>{space}</>
+        );
+        
+        let tdSpace=(<><td style={{borderRight:"1px solid grey", width:8}}>&nbsp;</td><td style={{width: 16}}>&nbsp;</td></>)
+        
+        
+        
+        
         return (
-            <div>
-                <p className="cs-group">Process:</p>
-                <p className="cs-line">[X] PID, [X] Name, [X] Priority, [X] Uptime, [X] Command line</p>
+            <>
 
-                <p className="cs-group">IO Time:</p>
-                <p className="cs-line">[X] Total, [X] Current</p>
+                <div className="cs-group">
+                    Process
+                </div>
+                <div className="cs-line">
+                    {chbox("PID")}
+                    {chbox("Name")}
+                    {chbox("User")}
+                    {chbox("Priority")}
+                    {chbox("Uptime")}
+                    {chbox("Command line")}
+                </div>
 
-                <p className="cs-group">IO Transfer:</p>
-                <p className="cs-line">Logical Read: [X] Total, [X] Current. Logical Write [X] Total, [X] Current</p>
-                <p className="cs-line">Calls Read: [X] Total, [X] Current. Calls Write: [X] Total, [X] Current</p>
-                <p className="cs-line">Block Level Read: [X] Total, [X] Current. Block Level Write: [X] Total, [X] Current</p>
+                <div className="cs-group">
+                    Memory
+                </div>
+                <div className="cs-line">
+                    {chbox("RSS")}
+                    {chbox("Shared")}
+                    {chbox("Swapped")}
+                </div>
 
-                <p className="cs-group">Memory:</p>
-                <p className="cs-line">[X] RSS, [X] Shared, [X] Swapped</p>
+                <div className="cs-group">
+                    IO Time
+                </div>
+                <div className="cs-line">
+                    {chbox("Total, hh:mm:ss;")}
+                    {chbox("Current, %%")}
+                </div>
 
-                <p className="cs-group">Page Faults:</p>
-                <p className="cs-line">Minor: [X] Total, [X] Current. Swapin: [X] Total, [X] Current</p>
-                <p className="cs-line">Children Minor: [X] Total, [X] Current. Children Swapins: [X] Total, [X] Current</p>
+                <div className="cs-group">
+                    IO Transfer
+                </div>
+                <div style={{position:"relative", height: 118, border: ""}}>
 
-                <p className="cs-group">CPU Usage:</p>
-                <p className="cs-line">User: [X] Total, [X] Current. Kernel: [X] Total, [X] Current. Total: [X] Total, [X] Current</p>
-                <p className="cs-line">Children User: [X] Total, [X] Current. Children Kernel: [X] Total, [X] Current. Children Total: [X] Total, [X] Current</p>
+                    <ABS x={0} y={0}>
+                        <FixedSpan width={120}>Logical Read:</FixedSpan> 
+                        {chbox("Total")} 
+                        {chbox("Current")}
+                    </ABS>
+                    <ABS x={4} y={0}>
+                        <FixedSpan width={128}>Logical Write:</FixedSpan>
+                        {chbox("Total")} 
+                        {chbox("Current")}
+                    </ABS>
 
-            </div>
+                    <ABS x={0} y={1}>
+                        <FixedSpan width={120}>Block-level Read:</FixedSpan>
+                        {chbox("Total")} 
+                        {chbox("Current")}
+                    </ABS>
+                    <ABS x={4} y={1}>
+                        <FixedSpan width={128}>Block-level Write:</FixedSpan>
+                        {chbox("Total")} 
+                        {chbox("Current")}
+                    </ABS>
+
+                    <ABS x={0} y={2}>
+                        <FixedSpan width={120}>Read Calls:</FixedSpan>
+                        {chbox("Total")} 
+                        {chbox("Current")}
+                    </ABS>
+                    <ABS x={4} y={2}>
+                        <FixedSpan width={128}>Write Calls:</FixedSpan> 
+                        {chbox("Total")} 
+                        {chbox("Current")}
+                    </ABS>
+
+                </div>
+
+                
+                <div className="cs-group">
+                    Page Faults
+                </div>
+                <div style={{position:"relative", height: 78, border: ""}}>
+
+                    <ABS x={0} y={0}>
+                        <FixedSpan width={120}>Minor:</FixedSpan>
+                        {chbox("Total")}
+                        {chbox("Current")}
+                    </ABS>
+                    <ABS x={4} y={0}>
+                        <FixedSpan width={128}>Swap-in:</FixedSpan>
+                        {chbox("Total")}
+                        {chbox("Current")}
+                    </ABS>
+
+                    <ABS x={0} y={1}>
+                        <FixedSpan width={120}>Children Minor:</FixedSpan>
+                        {chbox("Total")}
+                        {chbox("Current")}
+                    </ABS>
+                    <ABS x={4} y={1}>
+                        <FixedSpan width={128}>Children Swap-in:</FixedSpan>
+                        {chbox("Total")}
+                        {chbox("Current")}
+                    </ABS>
+
+                </div>
+
+                <div className="cs-group">
+                    CPU Usage
+                </div>
+                <div style={{position:"relative", height: 45, border: ""}}>
+
+                    <ABS3 x={0} y={0}>
+                        <FixedSpan width={40}>User:</FixedSpan>
+                        {chbox("Total")}
+                        {chbox("Current")}
+                    </ABS3>
+                    <ABS3 x={1} y={0}>
+                        <FixedSpan width={52}>Kernel:</FixedSpan>
+                        {chbox("Total")}
+                        {chbox("Current")}
+                    </ABS3>
+                    <ABS3 x={2} y={0}>
+                        <FixedSpan width={38}>Sum:</FixedSpan>
+                        {chbox("Total")}
+                        {chbox("Current")}
+                    </ABS3>
+
+                </div>
+
+                <div className="cs-group">
+                    Children CPU Usage
+                </div>
+                <div style={{position:"relative", height: 40, border: ""}}>
+
+                    <ABS3 x={0} y={0}>
+                        <FixedSpan width={40}>User:</FixedSpan>
+                        {chbox("Total")}
+                        {chbox("Current")}
+                    </ABS3>
+                    <ABS3 x={1} y={0}>
+                        <FixedSpan width={52}>Kernel:</FixedSpan>
+                        {chbox("Total")}
+                        {chbox("Current")}
+                    </ABS3>
+                    <ABS3 x={2} y={0}>
+                        <FixedSpan width={38}>Sum:</FixedSpan>
+                        {chbox("Total")}
+                        {chbox("Current")}
+                    </ABS3>
+
+                </div>
+
+            </>
         );
     }
 }
--- Universe.W3Top/ClientApp/src/components/ProcessList/ProcessListContainerV1.js
+++ Universe.W3Top/ClientApp/src/components/ProcessList/ProcessListContainerV1.js
@@ -12,6 +12,7 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 
 import "./ProcessList.css"
+import {ColumnChooserComponent} from "./ColumnChooserComponent";
 
 export class ProcessListContainerV1 extends Component {
     static displayName = ProcessListContainerV1.name;
@@ -69,28 +70,8 @@ export class ProcessListContainerV1 extends Component {
                         </Button>
                     </DialogTitle>
                     <DialogContent style={{textAlign_ignore: "center"}} id="columns-chooser-content">
-
-                        <p className="cs-group">Process:</p>                                        
-                        <p className="cs-line">[X] PID, [X] Name, [X] Priority, [X] Uptime, [X] Command line</p>
-
-                        <p className="cs-group">IO Time:</p>
-                        <p className="cs-line">[X] Total, [X] Current</p>
-
-                        <p className="cs-group">IO Transfer:</p>
-                        <p className="cs-line">Logical Read: [X] Total, [X] Current. Logical Write [X] Total, [X] Current</p>
-                        <p className="cs-line">Calls Read: [X] Total, [X] Current. Calls Write: [X] Total, [X] Current</p>
-                        <p className="cs-line">Block Level Read: [X] Total, [X] Current. Block Level Write: [X] Total, [X] Current</p>
-
-                        <p className="cs-group">Memory:</p>
-                        <p className="cs-line">[X] RSS, [X] Shared, [X] Swapped</p>
-
-                        <p className="cs-group">Page Faults:</p>
-                        <p className="cs-line">Minor: [X] Total, [X] Current. Swapin: [X] Total, [X] Current</p>
-                        <p className="cs-line">Children Minor: [X] Total, [X] Current. Children Swapins: [X] Total, [X] Current</p>
-
-                        <p className="cs-group">CPU Usage:</p>
-                        <p className="cs-line">User: [X] Total, [X] Current. Kernel: [X] Total, [X] Current. Total: [X] Total, [X] Current</p>
-                        <p className="cs-line">Children User: [X] Total, [X] Current. Children Kernel: [X] Total, [X] Current. Children Total: [X] Total, [X] Current</p>
+                        
+                        <ColumnChooserComponent />
                         
                     </DialogContent>
                         
