8cf38158568b0d167adf73d1d14fe5faf39dd131
devizer <devizer@github.io>
2020-06-21 16:32:53 +0300

    fix tests on non-linux again

--- KernelManagementJam.Tests/TaskStats_Tests.cs
+++ KernelManagementJam.Tests/TaskStats_Tests.cs
@@ -12,7 +12,7 @@ namespace KernelManagementJam.Tests
         public void Test_Pid()
         {
             if (CrossInfo.ThePlatform == CrossInfo.Platform.Linux)
-                Assert.AreEqual(Process.GetCurrentProcess().Id, TaskStatInterop.get_pid());
+                Assert.AreEqual(Process.GetCurrentProcess().Id, LinuxTaskStatsReader.GetPid());
             
         }
     }
