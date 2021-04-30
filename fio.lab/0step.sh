d976319c4c55eefc3b6cc8466980d15d3f0dabd7
devizer <devizer@users.github.io>
2021-04-30 21:57:30 +0300

    fio.lab: [1G], cancel timeout, 2.21 & 3.16 only, 2 libaio options, more archs, ***no_ci***

--- fio.lab/0step.sh
+++ fio.lab/0step.sh
@@ -1,1 +1,3 @@
 ﻿work=/transient-builds/fio; mkdir -p $work; cd $work; git clone https://github.com/devizer/KernelManagementLab; cd KernelManagementLab; git pull; cd fio.lab
+rm -rf result; mkdir -p result; time bash rebuild-all.sh | tee result/details.log
+
