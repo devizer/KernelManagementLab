b09f2ca779759610aa128662df17546810f34aae
devizer <devizer@users.github.io>
2021-05-04 16:16:04 +0300

    FioStreamReader.cs: Progress for ETA, Stage, Percents and IOPS, ***no_ci***

--- fio.lab/in-container.sh
+++ fio.lab/in-container.sh
@@ -21,6 +21,7 @@ cpus=$((cpus + 1))
 make -j${cpus} && make install
 mkdir -p /out
 rm -rf /out/*
+set -o pipefail
 if [[ -d /usr/local/fio ]]; then
     pushd /usr/local/fio
     tar czf /out/fio-distribution.tar.gz .
@@ -52,14 +53,11 @@ if [[ -d /usr/local/fio ]]; then
     # ? --status-interval=1
     export FILE_IO_BENCHMARK_OPTIONS="--eta=always --time_based"
     export FILE_IO_BENCHMARK_DUMP_FOLDER=/out/dumps
-    cat $(command -v File-IO-Benchmark)
-    set -o pipefail
     File-IO-Benchmark "CONTAINER" $(pwd) 1G 3 3 | tee /out/Benchmark.log
     exit_code=$?
     gzip -9 /out/Benchmark.log
     Say "EXIT CODE of File-IO-Benchmark: $exit_code"
-    ls -la $FILE_IO_BENCHMARK_DUMP_FOLDER
-    ls -la $FILE_IO_BENCHMARK_DUMP_FOLDER/*
+    # cd $FILE_IO_BENCHMARK_DUMP_FOLDER; cd *
     popd >/dev/null
 elif [[ -s /usr/local/bin/fio ]]; then
   # 2.0 & 2.1
@@ -76,9 +74,11 @@ elif [[ -s /usr/local/bin/fio ]]; then
     Say "Testing fio ..."
     export PATH="$(pwd):$PATH"
     export FILE_IO_BENCHMARK_OPTIONS="--eta=always --time_based --output=output"
+    export FILE_IO_BENCHMARK_DUMP_FOLDER=/out/dumps
     File-IO-Benchmark "CONTAINER" $(pwd) 1G 3 3 | tee /out/Benchmark.log
+    exit_code=$?
     gzip -9 /out/Benchmark.log
-    Say "EXIT CODE of File-IO-Benchmark: $?"
+    Say "EXIT CODE of File-IO-Benchmark: $exit_code"
   popd >/dev/null
   
 fi
