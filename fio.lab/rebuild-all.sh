075b514e9c2abc0f4f105e8ccba50b0181d67e1f
devizer <devizer@users.github.io>
2021-05-04 16:25:02 +0300

    FioStreamReader.cs: Progress for ETA, Stage, Percents and IOPS, ALL LOGS ***no_ci***

--- fio.lab/rebuild-all.sh
+++ fio.lab/rebuild-all.sh
@@ -66,8 +66,9 @@ function build() {
   options_commands=("${cmd_i1};" "${cmd_r1};")
   options_keys=("-libaio" "")
 
-  # options_commands=("${cmd_i1};")
-  # options_keys=("-testlog")
+  # ONLY libaio
+  options_commands=("${cmd_i1};")
+  options_keys=("-testlog")
   
   # cut next two lines
   # options_commands=("${cmd_i1};${cmd_i2}")
@@ -118,6 +119,8 @@ function build() {
 }
 
 build multiarch/ubuntu-debootstrap amd64-precise      amd64-precise       prepare_debian
+build multiarch/ubuntu-debootstrap amd64-focal        amd64-focal         prepare_debian
+exit;
 build ubuntu groovy                                   amd64-groovy        prepare_debian
 build centos 8                                        amd64-rhel8         prepare_centos
 build quay.io/centos/centos stream                    amd64-centosstream  prepare_centos_stream
@@ -126,7 +129,6 @@ build centos 6                                        amd64-rhel6         prepar
 
 build ubuntu hirsute                                  amd64-hirsute       prepare_debian
 
-build multiarch/ubuntu-debootstrap amd64-focal        amd64-focal         prepare_debian
 build multiarch/ubuntu-debootstrap amd64-xenial       amd64-xenial        prepare_debian
 
 build multiarch/debian-debootstrap amd64-stretch      amd64-stretch       prepare_debian
