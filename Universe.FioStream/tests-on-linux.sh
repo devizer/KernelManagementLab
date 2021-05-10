be874bd33ee6e645fbb83b15beb7587cac069e34
devizer <devizer@users.github.io>
2021-05-10 15:28:40 +0300

    FioStream: Linux Candidates, net 3.5 on linux ***no_ci***

--- Universe.FioStream/tests-on-linux.sh
+++ Universe.FioStream/tests-on-linux.sh
@@ -1,3 +1,3 @@
-work=/transient-builds/fio; mkdir -p $work; cd $work; git clone https://github.com/devizer/KernelManagementLab; cd KernelManagementLab; git pull; cd Universe.FioStream.Tests
+work=/transient-builds/fio; sudo mkdir -p $work; sudo chown -R $(whoami) $work; cd $work; git clone https://github.com/devizer/KernelManagementLab; cd KernelManagementLab; git pull; cd Universe.FioStream.Tests
 time dotnet test -f net5.0
 
