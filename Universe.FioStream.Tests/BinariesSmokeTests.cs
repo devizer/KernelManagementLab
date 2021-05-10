72de07fd42aa6c7eb9f4553ef54543a97b375f20
devizer <devizer@users.github.io>
2021-05-10 14:35:00 +0300

    FioStream: Linux Candidates ***no_ci***

--- Universe.FioStream.Tests/BinariesSmokeTests.cs
+++ Universe.FioStream.Tests/BinariesSmokeTests.cs
@@ -15,6 +15,17 @@ namespace Universe.FioStream.Tests
         public void SetUp() => FioStreamReader.ConsolasDebug = false;
 
         [Test]
+        public void Test_Linux_Candidates()
+        {
+            var binaries = OrderedLinuxCandidates.GetLinuxCandidates();
+                
+            foreach (var bin in binaries)
+            {
+                Console.WriteLine(bin);
+            }
+        }
+
+        [Test]
         [TestCase(true,  TestName = "1. No Cache")]
         [TestCase(false, TestName = "2. Allow Cache")]
         public void Test_Download_Only(bool ignoreCache)
@@ -32,7 +43,22 @@ namespace Universe.FioStream.Tests
                     var cached = d.CacheGZip(bin.Name, bin.Url);
                     Console.WriteLine($"  --> '{cached}', {sw.Elapsed}");
                 }
+            }
+            else if (CrossInfo.ThePlatform == CrossInfo.Platform.MacOSX)
+            {
+                var binaries = Candidates.AllMacOsCandidates();
                 
+                foreach (var bin in binaries)
+                {
+                    Console.WriteLine(bin.Name);
+                    Stopwatch sw = Stopwatch.StartNew();
+                    GZipCachedDownloader d = new GZipCachedDownloader();
+                    var cached = d.CacheGZip(bin.Name, bin.Url);
+                    Console.WriteLine($"  --> '{cached}', {sw.Elapsed}");
+                }
+            }
+            else 
+            {
             }
         }
     }
