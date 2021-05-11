07132ccb466c7b80bc5faaed0be324eb145c2e33
devizer <devizer@github.io>
2021-05-12 00:09:33 +0300

    PersistentState for fio binaries, ***no_ci***

--- Universe.FioStream.Tests/PersistentState_Tests.cs
+++ Universe.FioStream.Tests/PersistentState_Tests.cs
@@ -8,12 +8,23 @@ namespace Universe.FioStream.Tests
     public class PersistentState_Tests : NUnitTestsBase
     {
         [Test]
-        public void Test1()
+        public void Test_Strings()
         {
             var string1 = PersistentState.GetOrStore("key1-A/key2-B", () => "SS777");
             Assert.AreEqual("SS777", string1, "string1 is SS777");
             var string2 = PersistentState.GetOrStore("key1-A/key2-B", () => "Never Evaluated");
             Assert.AreEqual("SS777", string2, "string2 is SS777");
+        }
+
+        [Test]
+        [TestCase(false, TestName = "Bool=False")]
+        [TestCase(true, TestName = "Bool=True")]
+        public void Test_Strings(bool expected)
+        {
+            bool bool1 = PersistentState.GetOrStore($"test-bool/try-{expected}", () => expected);
+            Assert.AreEqual(expected, bool1, $"bool1 is {expected}");
+            bool bool2 = PersistentState.GetOrStore($"test-bool/try-{expected}", () => !expected);
+            Assert.AreEqual(expected, bool1, $"bool1 is {expected}");
         }
         
     }
