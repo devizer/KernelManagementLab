2f89ce0676c0507530bc86de9acccddda1d9ab36
devizer <devizer@users.github.io>
2021-05-26 12:40:56 +0300

    libaio ***no_ci***

--- fio.lab-v2/in-container-libaio.sh
+++ fio.lab-v2/in-container-libaio.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+set -e
 ver=$1
 echo "BUILDING LIBAIO $ver"
 cd /transient-builds/libaio-src/$ver/lib*
 rm -rf /transient-builds/libaio-dev || true
 time make prefix=/transient-builds/libaio-dev install
 echo '
-CFLAGS="-O2 -I/transient-builds/libaio-dev/include/"
-LDFLAGS="-L/transient-builds/libaio-dev/lib/"
+export CFLAGS="-O2 -I/transient-builds/libaio-dev/include/"
+export LDFLAGS="-L/transient-builds/libaio-dev/lib/"
+export LD_LIBRARY_PATH=/transient-builds/libaio-dev/lib
 ' > /vars.sh
