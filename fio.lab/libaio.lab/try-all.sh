1a9fc854b2c9912a012fb1b345ed449e432df00c
devizer <devizer@users.github.io>
2021-05-26 08:10:34 +0300

    libaio ***no_ci***

---
+++ fio.lab/libaio.lab/try-all.sh
@@ -0,0 +1,5 @@
+# work=/transient-builds/fio; sudo mkdir -p $work; sudo chown -R $(whoami) $work; cd $work; git clone https://github.com/devizer/KernelManagementLab; cd KernelManagementLab; git pull; cd fio.lab/libaio.lab
+work_base=/transient-builds/libaio-src
+cat ver-links.txt | while read -r ver link; do
+    echo "$ver" "$link"
+done 
