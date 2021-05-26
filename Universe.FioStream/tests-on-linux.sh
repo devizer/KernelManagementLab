cc2bdf418c3f02cc627f25bdfa2e9e2fbd45aaca
devizer <devizer@users.github.io>
2021-05-26 20:32:54 +0300

    libaio ***no_ci***

--- Universe.FioStream/tests-on-linux.sh
+++ Universe.FioStream/tests-on-linux.sh
@@ -1,5 +1,6 @@
 work=/transient-builds/fio; sudo mkdir -p $work; sudo chown -R $(whoami) $work; cd $work; git clone https://github.com/devizer/KernelManagementLab; cd KernelManagementLab; git pull; cd Universe.FioStream.Tests
 rm -rf ~/.local/var/fio-state
+time dotnet test  --filter "FullyQualifiedName~Test_Download_Only_Linux" -f net5.0 | tee test-discovery-fio-features.log
 time dotnet test  --filter "FullyQualifiedName~Discovery_Fio_Features" -f net5.0 | tee test-discovery-fio-features.log
 time dotnet test -f net5.0 | tee tests-all.log
 time dotnet test  --filter "FullyQualifiedName~Features_Tests" -f net5.0 | tee test-features.log
--- fio.lab-v2/rebuild-all.sh
+++ fio.lab-v2/rebuild-all.sh
@@ -158,6 +158,9 @@ function build() {
   docker rmi -f "${image}:${tag}"
 }
 
+build centos 6                                        amd64-rhel6         prepare_centos
+exit;
+
 build multiarch/ubuntu-debootstrap amd64-focal        amd64-focal         prepare_debian
 build multiarch/ubuntu-debootstrap amd64-precise      amd64-precise       prepare_debian
 
@@ -167,7 +170,6 @@ build ubuntu groovy                                   amd64-groovy        prepar
 build centos 8                                        amd64-rhel8         prepare_centos
 build quay.io/centos/centos stream                    amd64-centosstream  prepare_centos_stream
 build centos 7                                        amd64-rhel7         prepare_centos
-build centos 6                                        amd64-rhel6         prepare_centos
 
 
 build multiarch/ubuntu-debootstrap amd64-xenial       amd64-xenial        prepare_debian
