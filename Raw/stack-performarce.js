d9911d88c12200e77e0281fcc9f2666bfc74944a
devizer <devizer@users.github.io>
2021-06-23 18:25:07 +0300

    added stack benchmark (150-200 thousands a sec)

---
+++ Raw/stack-performarce.js
@@ -0,0 +1,17 @@
+﻿function benchmark(title,func) {
+  const durationLimit = 2000;
+  func();
+  let startAt = (window.performance) ? window.performance.now() : +new Date();
+  let duration = 0, count = 0;
+  while(duration < durationLimit) {
+    const batch = 1234;
+    for(let i=0; i<batch; i++) { func(); }
+    count += batch;
+    const stopwatch = (window.performance) ? window.performance.now() : +new Date();
+    duration = stopwatch - startAt;
+  }
+  function format(num) { return (Math.round(num * 100.0) / 100.0).toLocaleString(undefined, {useGrouping: true}); }
+  console.log(`%c%s PERFORMANCE: %s op/s`, 'color: #146D2B', title, format(count * 1000.0 / duration));
+}
+benchmark("[Error.stack] ", function() { let stack = new Error().stack });
+benchmark("[No Operation]", function() { let ok = "sure" });
