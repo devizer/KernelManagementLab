4c5857172eda563c3405451feff517ce1f98c39f
devizer <devizer@github.users>
2021-12-24 08:37:18 +0200

    addded alt arm tests v3

--- azure-vm-tasks.sh
+++ azure-vm-tasks.sh
@@ -17,10 +17,25 @@ source "$api_code_file"
 
 DownloadVM $VM_KEY
 
+cmd='
+echo starting in $(pwd); 
+cd ~; git clone https://github.com/devizer/KernelManagementLab; pwd; uname -a
+cd KernelManagementLab
+Say "Install NET Core 6.0 & 3.1.120"
+export DOTNET_VERSIONS="3.1.120 6.0" DOTNET_TARGET_DIR=/usr/share/dotnet
+script=https://raw.githubusercontent.com/devizer/test-and-build/master/lab/install-DOTNET.sh; 
+(wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash; 
+test -s /usr/share/dotnet/dotnet && sudo ln -f -s /usr/share/dotnet/dotnet /usr/local/bin/dotnet
+dotnet restore -v:m || (e=$?; Say "Error $e. Faullback restore"; kill_msbuild_service; dotnet restore -v:m --disable-parallel)
+exi=$?; Say "Final Restore status: $exi"
+dotnet test --collect:"XPlat Code Coverage" --logger trx -f netcoreapp3.1 -c Release -- NUnit.NumberOfTestWorkers=1
+e=$?
+Say "TEST STATUS: $e"
+'
 export VM_SSH_PORT=2207
 RunVM $VM_KEY
 Say "VM_ROOT_FS is [$VM_ROOT_FS]"
-EvaluateCommand "echo starting in $(pwd); cd ~; git clone https://github.com/devizer/KernelManagementLab; pwd; uname -a"
+EvaluateCommand "$cmd"
 ls -la $VM_ROOT_FS
 ShutdownVM $VM_KEY
 
