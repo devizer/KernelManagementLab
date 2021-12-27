357fb28252b71a6753ec93abfdd0ecc5953ad769
devizer <devizer@github.users>
2021-12-27 02:41:22 +0200

    1 test worker on arm

--- azure-vm-tasks.sh
+++ azure-vm-tasks.sh
@@ -60,8 +60,8 @@ test -s $file && export CURL_CA_BUNDLE="$file"
 Say "env"
 printenv | sort
 
-Say "dotnet test --blame -f netcoreapp3.1 -c Release --logger trx"
-time dotnet test --blame -f netcoreapp3.1 -c Release --logger trx
+Say "dotnet test --blame -f netcoreapp3.1 -c Release --logger trx -- NUnit.NumberOfTestWorkers=1"
+time dotnet test --blame -f netcoreapp3.1 -c Release --logger trx -- NUnit.NumberOfTestWorkers=1
 e=$?
 echo $e > tests-exit-code
 Say "TEST STATUS: $e"
