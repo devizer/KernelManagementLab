8ca5100dad7ab056245b27ff05440fd758081e96
devizer <devizer@users.github.io>
2022-03-30 04:50:45 -0400

    FirstRoundTest

--- KernelManagementJam.Tests/HugeCrossInfo_Tests.cs
+++ KernelManagementJam.Tests/HugeCrossInfo_Tests.cs
@@ -30,5 +30,17 @@ namespace KernelManagementJam.Tests
             Console.WriteLine($"Total Memory: [{HugeCrossInfo.TotalMemory:n0}]");
             Assert.IsTrue(HugeCrossInfo.TotalMemory.GetValueOrDefault() > 0);
         }
+
+        [Test]
+        public void FirstRoundTest()
+        {
+            int y1 = 0, y2 = 0, y3 = 0;
+            FirstRound.RunOnce(() => y1 = 1, "test-1");
+            FirstRound.RunOnly(() => y2 = 2, 1, "test-2");
+            FirstRound.RunTwice(() => y3 = 3, "test-3");
+            Assert.AreEqual(1, y1);
+            Assert.AreEqual(2, y2);
+            Assert.AreEqual(3, y3);
+        }
     }
 }
